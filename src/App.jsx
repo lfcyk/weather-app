ded21a9358e2e68b3e42abe55aa08794d989f287
Willa Waliyadin <59253075+lfcyk@users.noreply.github.com>
2023-10-24 20:19:04 +0700

    fix: use search icon for button

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import * as React from 'react';
 import { TiWeatherPartlySunny } from 'react-icons/ti';
+import { BsSearch } from 'react-icons/bs'
 
 
 function App() {
+	const [searchTerm, setSearchTerm] = React.useState("");
+	const handleChange = event => {
+	  setSearchTerm(event.target.value);
+	};
 
+	const onSearchClick = () => {
+		console.log(searchTerm);
+	}
 return (
 	<>
-		<div class="flex flex-col items-center">
-			<div class="flex flex-row text-6xl font-bold gap-3 mx-auto max-w-fit items-center mt-20">
+		<div className="flex flex-col items-center">
+			<div className="flex flex-row text-6xl font-bold gap-3 mx-auto max-w-fit items-center mt-20">
 				<TiWeatherPartlySunny/>
-				<div class="text-4xl max-w-fit">Weather App</div>
+				<div className="text-4xl max-w-fit">Weather App</div>
 			</div>
 
-			<div class="flex flex-row mt-10 gap-2">
-				<input type="text" placeholder="Enter location here" className="input input-bordered min-w-[400px] max-w-xs " />
-				<button className="btn btn-accent">Search</button>
+			<div className="flex flex-row mt-10 gap-2">
+				<input type="text" placeholder="Enter location here" className="input input-bordered min-w-[400px] max-w-xs" value={searchTerm} onChange={handleChange} />
+				<button className="btn btn-accent" onClick={onSearchClick}><BsSearch/></button>
 			</div>
 		</div>
 		
